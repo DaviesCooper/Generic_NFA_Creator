936f54ce08eda5a21920413a5f5aaa81a3ad3748
Cooper Davies <ctdavies@ucalgary.ca>
2017-09-28 11:32:12 -0600

    Add files via upload

---
+++ State.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace tileRead.Datastructures.StateMachine
+{
+    /// <summary>
+    /// interface for generating states.
+    /// Because states can be general things (i.e. locations or colour values etc.) we need this to be generic
+    /// </summary>
+    public interface State : IEquatable<State>
+    {
+        //Used so we can identify states
+        int GetHashCode();
+        //Useful for debugging
+        string ToString();
+    }
+
+    public class Transition<T> : IEquatable<Transition<T>> where T :  new()
+    {
+        public T transitionValue;
+        private HashSet<State> nextStates;
+
+        /// <summary>
+        /// checks if the transition value is the same
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Transition<T> other)
+        {
+            if (other == null)
+                return false;
+            //check if they are literally the same object
+            if (ReferenceEquals(this, other))
+                return true;
+            if (other.transitionValue.Equals(transitionValue))
+                return true;
+            return false;
+        }
+
+        public Transition() : this(new T()){}
+        public Transition(T value)
+        {
+            transitionValue = value;
+            nextStates = new HashSet<State>();
+        }
+        public Transition(T value, State next)
+        {
+            transitionValue = value;
+            nextStates = new HashSet<State>();
+            nextStates.Add(next);
+        }
+
+        public void addTransition(State next)
+        {
+            nextStates.Add(next);
+        }
+
+        public List<State> getStatesAsList()
+        {
+            return nextStates.ToList();
+        }
+        public State[] getStatesAsArray()
+        {
+            return nextStates.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+    public class TransitionState<T> : IEquatable<TransitionState<T>> where T : new()
+    {
+
+        //The state we are "coming" from
+        public State currentState;
+        //The transition values from this state
+        public HashSet<Transition<T>> transitions;
+
+
+        public TransitionState(State cur)
+        {
+            currentState = cur;
+            transitions = new HashSet<Transition<T>>();
+        }
+
+        /// <summary>
+        /// public constructor generates an empty state list for the next possible states
+        /// </summary>
+        /// <param name="cur"></param>
+        /// <param name="tVal"></param>
+        public TransitionState(State cur, T tVal)
+        {
+            currentState = cur;
+            transitions = new HashSet<Transition<T>>();
+            transitions.Add(new Transition<T>(tVal));
+        }
+
+        public TransitionState(State cur, T tVal, State fin)
+        {
+            currentState = cur;
+            transitions = new HashSet<Transition<T>>();
+            transitions.Add(new Transition<T>(tVal, fin));
+        }
+
+        /// <summary>
+        /// Add a new transition to this current state transition.
+        /// This is essentially what makes this an NFA right here.
+        /// </summary>
+        /// <param name="newTrans"></param>
+        public void addTransition(T transitionVal, State nextState)
+        {
+            if(transitions.Contains(new Transition<T>(transitionVal)))
+            {
+                transitions.First(x => x.transitionValue.Equals(transitionVal)).addTransition(nextState);
+            }
+            else
+            {
+                transitions.Add(new Transition<T>(transitionVal, nextState));
+            }
+        }
+
+        /// <summary>
+        /// Call to get the list of states that this state transition is capable of producing
+        /// </summary>
+        /// <returns></returns>
+        public State[] getNextStates()
+        {
+            if(transitions == null)
+            {
+                return new State[0];
+            }
+            HashSet<State> retVal = new HashSet<State>();
+            foreach(Transition<T> trans in transitions)
+            {
+                foreach(State s in trans.getStatesAsList())
+                {
+                    retVal.Add(s);
+                }
+            }
+            return retVal.ToArray();
+        }
+
+
+        /// <summary>
+        /// Returns all possible T values that can be passed in to cause this state to transition.
+        /// </summary>
+        /// <returns></returns>
+        public T[] getPossibleTransitionValue()
+        {
+            if(transitions == null)
+            {
+                return new T[0];
+            }
+            T[] retVal = new T[transitions.Count];
+            int count = 0;
+            foreach(Transition<T> trans in transitions)
+            {
+                retVal[count] = trans.transitionValue;
+                count++;
+            }
+            return retVal;
+        }
+
+        /// <summary>
+        /// Returns all states that a possible transition might generate (because this is an nfa)
+        /// </summary>
+        /// <returns></returns>
+        public State[] getNextStatesForTransition(T transitionValue)
+        {
+            if (transitions == null)
+            {
+                return new State[0];
+            }
+            HashSet<State> retVal = new HashSet<State>();    
+            //filtering for transition value
+            foreach (Transition<T> trans in transitions.Where(x => x.transitionValue.Equals(transitionValue)))
+            {
+                foreach (State s in trans.getStatesAsList())
+                {
+                    retVal.Add(s);
+                }
+            }
+            return retVal.ToArray();
+        }
+
+        /// <summary>
+        /// If the state this transition starts at is that same as the state the passed in transition starts from
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(TransitionState<T> other)
+        {
+            if (other == null)
+                return false;
+            //check if they are literally the same object
+            if (ReferenceEquals(this, other))
+                return true;
+            if (other.currentState.Equals(currentState))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// A very functional programming way of string joining
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ StateMachine.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using tileRead.Datastructures.StateMachine;
+using tileReadTest.Datastructures;
+
+namespace tileRead.Datastructures
+{
+    public class NFAException : Exception
+    {
+        public NFAException()
+            : base() { }
+
+        public NFAException(string message)
+            : base(message) { }
+
+        public NFAException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public NFAException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public NFAException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+    }
+
+
+    /// <summary>
+    /// An NFA machine.
+    /// (Q, Sigma, Delta, q0, F)
+    /// (set of all states, input symbols, transition functions, start state, final state(singular because our data only HAS one final state)
+    /// </summary>
+    /// <typeparam name="T">Sigma</typeparam>
+    public class StateMachine<T> where T : new()
+    {
+        #region Delegate Definitions
+        /// <summary>
+        /// The Delta of the NFA machine
+        /// Delegate for creating the transition table.
+        /// Given a set of entries, defines how to generate states for the entries as well as the state transitions and the relationship between the two
+        /// </summary>
+        /// <param name="entries"></param>
+        /// <returns></returns>
+        public delegate HashSet<TransitionState<T>> generateTransitionTableFromEntries(Object[] entries);
+
+        /// <summary>
+        /// Delegate for creating States from entries
+        /// used for generating (q ∈ Q)
+        /// </summary>
+        /// <param name="toConvert"></param>
+        /// <returns></returns>
+        public delegate State generateStateFromEntry(Object toConvert);
+
+        /// <summary>
+        /// Delegate for deciding how to choose between multiple posible states correspondiong to a state transitions state change
+        /// </summary>
+        /// <param name="possibleOuts">the possible states to transition to</param>
+        /// <returns></returns>
+        public delegate State tieBreak(State[] possibleOuts);
+        #endregion
+
+        //The only public var as its the only one we will be referencing for our visualizations
+        public State currentState;
+        private State previousState;
+        //q0
+        private State startState;
+        //F
+        private State finalState;
+        public HashSet<TransitionState<T>> transitionTable;
+
+
+        /// <summary>
+        /// Creates the machine.
+        /// Works in a very functional programming way in that it has two higher order functions being used as arguments
+        /// </summary>
+        /// <param name="entries">the array of all entries the machine is to be generated from</param>
+        /// <param name="tableGenerator">a delegate methid</param>
+        /// <param name="stateGenerator"></param>
+        public StateMachine(Entry[] entries, ref generateTransitionTableFromEntries tableGenerator, ref generateStateFromEntry stateGenerator)
+        {
+            transitionTable = tableGenerator(entries);
+            startState = stateGenerator(entries[0]);
+            finalState = stateGenerator(entries[entries.Length - 1]);
+        }
+
+        /// <summary>
+        /// Places our current state as the start state
+        /// </summary>
+        public void StartMachine()
+        {
+            currentState = startState;
+        }
+
+        /// <summary>
+        /// Get the transitionstate structure which contains all the transition structures possible for this state
+        /// </summary>
+        /// <returns></returns>
+        private TransitionState<T> getTransitionForCurrentState()
+        {
+            if (currentState == null)
+            {
+                throw new NFAException("There does not exist a current state currently");
+            }
+            //returns all elements in the hashset which have the same current state as us
+            return transitionTable.FirstOrDefault(x => x.currentState == currentState);
+        }
+
+        /// <summary>
+        /// returns all possible transition values that cause this state to transition to another.
+        /// If a value does not cause this state to change, it will not be returned
+        /// </summary>
+        /// <returns></returns>
+        private T[] getPossibleTransitionValuesForCurrentState()
+        {
+            try
+            {
+                TransitionState<T> trans = getTransitionForCurrentState();
+                return trans.getPossibleTransitionValue();
+            }
+            catch (Exception)
+            {
+                throw new NFAException("It is not possible to transition out of our current state");
+            }
+        }
+
+        /// <summary>
+        /// gets the group of next states for the current state given a transition value
+        /// </summary>
+        /// <param name="transitionValue"></param>
+        /// <returns></returns>
+        private State[] GetNextStates(T transitionValue)
+        {
+            TransitionState<T> transitions = getTransitionForCurrentState();
+            State[] states = transitions.getNextStatesForTransition(transitionValue);
+            if (states.Length < 1)
+                throw new NFAException("There did not exist a transition of " + transitionValue.ToString() + " for the current state");
+            else
+                return states;
+        }
+
+        /// <summary>
+        /// Takes the machine from its current state to the next state based on this input.
+        /// Because this is an NFA, there is a delegate here for how the user would like 
+        /// to choose between multiple states for the next state should multiple states
+        /// be an option for the given input.
+        /// Returns true if a final state has been reached
+        /// </summary>
+        /// <param name="input"></param>
+        public bool Transition(T input, ref tieBreak tieBreakers)
+        {
+            //set our previous state to be this one
+            previousState = currentState;
+            //and use our delegate to decide how we want to choose our next state
+            currentState = tieBreakers(GetNextStates(input));
+            return checkIfFinalState();
+        }
+
+        /// <summary>
+        /// Used for determining if we have reached the final state
+        /// </summary>
+        /// <returns></returns>
+        private bool checkIfFinalState()
+        {
+            return finalState.Equals(currentState);
+        }
+    }
+}
